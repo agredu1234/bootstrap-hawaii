aada3642910947bb2c9adce19c1c3b1b26fe6205
Eduardo <agredu1234@gmail.com>
2020-03-19 15:22:42 -0500

    proyecto hawaii 1

---
+++ index.js
@@ -0,0 +1,6 @@
+$(function(){
+    $('[data-toggle="tooltip"]').tooltip()
+})
+/* agarrar todos los elemento que tengan el atributo 
+data-toggle="tooltip" y vamos a llamar a la funcion tooltip 
+sobre ellos*/
